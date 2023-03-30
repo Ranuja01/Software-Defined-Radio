c6d924ed555d59cc1f1b3956f11e341afa738839
wangp56 <90365245+wangp56@users.noreply.github.com>
2023-03-30 14:04:03 -0400

    Update iofunc.h

--- include/iofunc.h
+++ include/iofunc.h
@@ -7,8 +7,8 @@
 #include <vector>
 
 //void write_stereo_data(std::vector<float>& , float, std::vector<short int>&);
-void write_stereo_data(std::vector<float> &audio, float audio_Fs, std::vector<short int> &play);
+void write_stereo_data(std::vector<float> &audio, float audio_Fs);
 void read_audio_data(const std::string in_fname, std::vector<uint8_t> &audio_data);
-void write_audio_data(std::vector<float> &audio, float audio_Fs, std::vector<short int> &play);
+void write_audio_data(std::vector<float> &audio, float audio_Fs);
 
 #endif // DY4_IOFUNC_H
